d2b9488aefb1b414b6d0f035e28d4730e3d9811e
Dina Berry <dinaberry@outlook.com>
2014-10-09 12:36:22 -0700

    username and password passed as param 1 and 2 for github

--- scripts/GetMerchello-Documentation.sh
+++ scripts/GetMerchello-Documentation.sh
@@ -5,9 +5,27 @@
 #
 # assumes git init
 # assumes git remote add origin https://user:password@github.com/rustyswayne/ExamineMd.git
+#
+# %1 username
+# %2 password
+#
+# throws error message is 2 params aren't passed
 ###########################################
 #git init
-#git remote add origin https://user:password@github.com/rustyswayne/ExamineMd.git
+git remote add origin https://$1:$2@github.com/Merchello/Merchello-Documentation.git
+
+if [ $# -eq 2 ]; then
+
+cd ../..
+cd Merchello-Documentation
 git pull origin master
 git merge origin/master
 git checkout -f
+
+else
+
+	echo Username and Password were not passed in
+
+fi
+
+exit
